da85f48751eee96e08140592b02db564d1b625ac
Maria <70840243+babe666@users.noreply.github.com>
2025-02-12 16:10:32 +0300

    Add files via upload
    Функции

---
+++ week9/script.js
@@ -0,0 +1,10 @@
+const firstImg = document.querySelector('.bouquet');
+const showPrevious = () => {
+    firstImg.src = "./roses.jpg"
+};
+
+const secondImg = document.querySelector('.bouquet');
+const showNext = () => {
+    secondImg.src = "./khrizantema.jpg"
+    secondImg.src = "./assorti.jpg"
+};
